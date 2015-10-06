b9c6c47d9d72b2cf5902e7311854c618bb138d72
Troy <bbqchickenrobot@gmail.com>
2015-10-06 03:00:09 -0700

    Switched to FreshMVVM - Compiling

--- samples/FreshMvvmSampleApp/Droid/MainActivity.cs
+++ samples/FreshMvvmSampleApp/Droid/MainActivity.cs
@@ -10,7 +10,8 @@ using Android.OS;
 
 namespace FreshMvvmSampleApp.Droid
 {
-	[Activity (Label = "FreshMvvmSampleApp.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "FreshMvvmSampleApp.Droid", Icon = "@drawable/icon", MainLauncher = true, 
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
